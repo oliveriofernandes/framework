cfe06e61341f4e1e1871ff9098c16d6c3e5317b5
oliverio <oliverio@oliverio-pc>
2013-12-06 09:51:57 -0200

    Implementing test2

---
+++ src/agentes/BehaviourTest2.java
@@ -0,0 +1,16 @@
+package agentes;
+
+import jade.core.Agent;
+import jade.core.behaviours.ReceiverBehaviour;
+
+public class BehaviourTest2 extends ReceiverBehaviour {
+
+	public BehaviourTest2(Agent a, long period) {
+		super();
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	
+
+}
